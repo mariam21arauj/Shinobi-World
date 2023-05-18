c4b1d7f087d837e0dfeebee4f94d57a1b42a45e0
Mariam Araujo <mariam21betancourt@gmail.com>
2023-05-18 15:58:36 -0700

    adds utilitie folders and files

--- src/pages/AuthPage/AuthPage.jsx
+++ src/pages/AuthPage/AuthPage.jsx
@@ -1,5 +1,11 @@
+import SignUpForm from "../../components/SignUpForm/SignUpForm";
+
 export default function AuthPage(){
     return(
-        <h1>AuthPage</h1>
+        <main>
+             <h1>AuthPage</h1>
+             <SignUpForm/>
+        </main>
+       
     )
 }
