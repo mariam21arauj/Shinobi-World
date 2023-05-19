b5971d8b8660a6ad5dbdaf816a17bb17e2277574
Mariam Araujo <mariam21betancourt@gmail.com>
2023-05-19 03:24:51 -0700

    minor fix

--- src/pages/VillageDetailPage/VillageDetailPage.jsx
+++ src/pages/VillageDetailPage/VillageDetailPage.jsx
@@ -44,7 +44,6 @@ export default function VillageDetailPage(props){
             )
         } else {
             // TODO: fix this conditional so that villages with an empty array of characters displays this message //
-            if(charactersExists = []){
                 return (
                     <div>
                         <h2>{villageDetail.name}</h2>
@@ -53,7 +52,7 @@ export default function VillageDetailPage(props){
                         </div>
                     </div>
                 )
-            }
+            
         } 
     }
             
