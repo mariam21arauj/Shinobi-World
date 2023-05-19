c88ad44dd44be688f2204df0d61eaedcde1cea0d
Mariam Araujo <mariam21betancourt@gmail.com>
2023-05-19 00:54:15 -0700

    adds css to main page

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,21 +1,28 @@
 import { Link } from "react-router-dom";
+import "./Main.css"
 
 export default function Main(props){
     return(
         <section className="main-page">
-            <h1>Welcome to Shinobi World</h1>
-            <article>
+            <h1 id='title'>Welcome to Shinobi World</h1>
+            <article className="tribute-info1">
                 Welcome, User. This app is designed to help you navigate the Naruto World
                 and learn more about their characters, the Clans, Villages, and Tailed Beasts.
             </article>
-            <div>
-            <Link to="/characters">Characters</Link>
-            &nbsp; | &nbsp;
-            <Link to="/villages">Villages</Link>
-            &nbsp; | &nbsp;
-            <Link to="/tailedbeasts">Tailed Beasts</Link>
-            &nbsp; | &nbsp;
-            <Link to="/clans">Clans</Link>
+            <div id="img-div" className="relative">
+                <div id="img-control" className="relative">
+                <img src="https://i.postimg.cc/Wz0b7h39/naruto.png" alt="tribute_image" id="image"/>
+                </div>
+                <figcaption id="img-caption">..</figcaption>
+            </div>
+            <div className="urls">
+            <div id="tribute-info" ><Link id="tribute-link" to="/characters">Characters</Link></div>
+           
+            <div id="tribute-info" ><Link id="tribute-link" to="/villages">Villages</Link></div>
+            
+            <div id="tribute-info"><Link id="tribute-link" to="/tailedbeasts">Tailed Beasts</Link></div>
+          
+            <div id="tribute-info" ><Link id="tribute-link" to="/clans">Clans</Link></div>
             </div>
         </section>
     )
